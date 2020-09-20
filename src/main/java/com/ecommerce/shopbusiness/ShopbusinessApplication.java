cadfbfbc4d54f1a011be1d0155112aa6d5e61512
Binu <binuadhikari@MacBook-Pro-de-Binu.local>
2020-09-20 06:39:28 +0200

    Initial commit

---
+++ src/main/java/com/ecommerce/shopbusiness/ShopbusinessApplication.java
@@ -0,0 +1,13 @@
+package com.ecommerce.shopbusiness;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShopbusinessApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShopbusinessApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ecommerce/shopbusiness/ShopbusinessApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ecommerce.shopbusiness;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShopbusinessApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
